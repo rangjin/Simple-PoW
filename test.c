09d11c9e0f679418fba230d6db442a47d65c477b
rangjin <46679581+rangjin@users.noreply.github.com>
2023-04-07 01:25:39 +0900

    update test.c

--- test.c
+++ test.c
@@ -13,13 +13,15 @@ unsigned int proofOfWork(const char *data, int target) {
         sprintf(input, "%s%u", data, nonce);
         SHA256(input, strlen(input), hash);
 
-        int state = 1;
-        // printf("%d: ", nonce);
-        for (int i = 0; i < target; i++) {
-            // printf("%02x", hash[i]);
-            if (hash[i] != 0) {
+        int state = 1, k = target / 2;
+        for (int i = 0; i < k; i++) {
+            if (hash[i] != 0x00) {
                 state = 0;
             }
+        }
+
+        if (target % 2 != 0 && hash[k] > 0x0F) {
+            state = 0;
         }
 
         if (state) {
@@ -36,14 +38,14 @@ unsigned int proofOfWork(const char *data, int target) {
 
 int main() {
     const char *data = "201928492019284920192849";
-    int target = 3;
+    int target = 8;
     double timeSpent = 0.0;
+
     time_t begin = time(NULL);
     unsigned int nonce = proofOfWork(data, target);
-
     time_t end = time(NULL);
 
-    printf("nonce: %u, time spent: %lf\n", nonce, (double) (end - begin));
+    printf("\nnonce: %u, time spent: %lf\n", nonce, (double) end - (double) begin);
 
     return 0;
 }
