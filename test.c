16fac087abb59087f582697cee234f7de9bd326e
rangjin <46679581+rangjin@users.noreply.github.com>
2023-04-07 15:35:56 +0900

    nonce 타입을 unsigned long long 으로 변경

--- test.c
+++ test.c
@@ -4,13 +4,13 @@
 #include <string.h>
 #include <openssl/sha.h>
 
-unsigned int proofOfWork(const char *data, int target) {
-    unsigned int nonce;
-    char input[SHA256_DIGEST_LENGTH + sizeof(unsigned int)];
+unsigned long long proofOfWork(const char *data, int target) {
+    unsigned long long nonce;
+    char input[SHA256_DIGEST_LENGTH + sizeof(unsigned long long)];
     unsigned char hash[SHA256_DIGEST_LENGTH];
 
-    for (nonce = 0; nonce < UINT_MAX; nonce++) {
-        sprintf(input, "%s%u", data, nonce);
+    for (nonce = 0; nonce < ULLONG_MAX; nonce++) {
+        sprintf(input, "%s%llu", data, nonce);
         SHA256(input, strlen(input), hash);
 
         int state = 1, k = target / 2;
@@ -25,7 +25,7 @@ unsigned int proofOfWork(const char *data, int target) {
         }
 
         if (state) {
-            printf("%d: ", nonce);
+            printf("%llu: ", nonce);
             for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
                 printf("%02x", hash[i]);
             }
@@ -42,10 +42,24 @@ int main() {
     double timeSpent = 0.0;
 
     time_t begin = time(NULL);
-    unsigned int nonce = proofOfWork(data, target);
+    unsigned long long nonce = proofOfWork(data, target);
     time_t end = time(NULL);
 
-    printf("\nnonce: %u, time spent: %lf\n", nonce, (double) end - (double) begin);
+    printf("\nnonce: %llu, time spent: %lf\n", nonce, (double) end - (double) begin);
 
     return 0;
 }
+
+/*
+
+201928492019284920192849
+
+target = 7
+76274466: 0000000fa5ae90e92086f6f84a8bcce56217040f872830ce26499df9d1ac4dfb
+nonce: 76274466, time spent: 21.000000
+
+target = 8
+8322403718: 000000008c651f58b3076c78ad324a7ea0b5824fe04da0cf05b2092d632b31ea
+nonce: 8322403718, time spent: 2371.000000
+
+*/
