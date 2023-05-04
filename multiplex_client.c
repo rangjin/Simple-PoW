328ec2dd47a656e4bff8ca091e80c8067ec64d94
rangjin <46679581+rangjin@users.noreply.github.com>
2023-05-05 01:37:14 +0900

    multiplex 사용

---
+++ multiplex_client.c
@@ -0,0 +1,187 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Lewis Van Winkle
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <openssl/sha.h>
+
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+
+unsigned long long proofOfWork(const char *data, int target, unsigned long long start) {
+    unsigned long long nonce;
+    char input[SHA256_DIGEST_LENGTH + sizeof(unsigned long long)];
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+
+    for (nonce = start; nonce < start + 100000; nonce++) {
+        sprintf(input, "%s%llu", data, nonce);
+        SHA256(input, strlen(input), hash);
+
+        int state = 1, k = target / 2;
+        for (int i = 0; i < k; i++) {
+            if (hash[i] != 0x00) {
+                state = 0;
+            }
+        }
+
+        if (target % 2 != 0 && hash[k] > 0x0F) {
+            state = 0;
+        }
+
+        if (state) {
+            printf("%llu: ", nonce);
+            for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+                printf("%02x", hash[i]);
+            }
+            printf("\n");
+            return nonce;
+        }
+    }
+
+    return 0;
+}
+
+
+int main(int argc, char *argv[]) {
+    char data[BUFSIZ], targetBuff[BUFSIZ];
+    int dataLen, targetLen, target;
+
+    if (argc < 3) {
+        fprintf(stderr, "usage: tcp_client hostname port\n");
+        return 1;
+    }
+
+    printf("Configuring remote address...\n");
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_socktype = SOCK_STREAM;
+    struct addrinfo *peer_address;
+    if (getaddrinfo(argv[1], argv[2], &hints, &peer_address)) {
+        fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    printf("Remote address is: ");
+    char address_buffer[100];
+    char service_buffer[100];
+    getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen,
+            address_buffer, sizeof(address_buffer),
+            service_buffer, sizeof(service_buffer),
+            NI_NUMERICHOST);
+    printf("%s %s\n", address_buffer, service_buffer);
+
+    printf("Creating socket...\n");
+    SOCKET socket_peer;
+    socket_peer = socket(peer_address->ai_family,
+            peer_address->ai_socktype, peer_address->ai_protocol);
+    if (!ISVALIDSOCKET(socket_peer)) {
+        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    printf("Connecting...\n");
+    if (connect(socket_peer,
+                peer_address->ai_addr, peer_address->ai_addrlen)) {
+        fprintf(stderr, "connect() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+    freeaddrinfo(peer_address);
+
+    printf("Connected.\n");
+
+    dataLen = read(socket_peer, data, sizeof(data));
+    data[dataLen] = '\0';
+
+    targetLen = read(socket_peer, targetBuff, sizeof(targetBuff));
+    targetBuff[targetLen] = '\0';
+    target = atoi(targetBuff);
+
+    printf("%s, %d\n", data, target);
+
+    char rBuff[BUFSIZ], wBuff[BUFSIZ];
+    unsigned long long ans, start;
+
+    while(1) {
+        fd_set reads;
+        FD_ZERO(&reads);
+        FD_SET(socket_peer, &reads);
+        FD_SET(0, &reads);
+
+        struct timeval timeout;
+        timeout.tv_sec = 0;
+        timeout.tv_usec = 100000;
+
+        if (select(socket_peer+1, &reads, 0, 0, &timeout) < 0) {
+            fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
+            return 1;
+        }
+
+        if (FD_ISSET(socket_peer, &reads)) {
+            int readLen = read(socket_peer, rBuff, sizeof(rBuff));
+            if (readLen < 1) {
+                printf("Connection closed by peer.\n");
+                break;
+            }
+            rBuff[readLen] = '\0';
+
+            start = strtoull(rBuff, 0, 10);
+            printf("%lld\n", start);
+            ans = proofOfWork(data, target, start);
+
+            if (ans == 0ULL) {
+                sprintf(wBuff, "N %lld", start);
+                write(socket_peer, wBuff, strlen(wBuff));
+            } else {
+                sprintf(wBuff, "Y %lld", ans);
+                write(socket_peer, wBuff, strlen(wBuff));
+            }
+        }
+
+        if (FD_ISSET(0, &reads)) {
+            char read[4096];
+            if (!fgets(read, 4096, stdin)) break;
+            if (strncmp(read, "exit()", 6) == 0) {
+                break;
+            }
+            send(socket_peer, read, strlen(read), 0);
+        }
+    }
+
+    printf("Closing socket...\n");
+    CLOSESOCKET(socket_peer);
+
+    printf("Finished.\n");
+    return 0;
+}
+
---
+++ multiplex_server.c
@@ -0,0 +1,151 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Lewis Van Winkle
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+
+int main() {
+    printf("Configuring local address...\n");
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    struct addrinfo *bind_address;
+    getaddrinfo(0, "8080", &hints, &bind_address);
+
+    printf("Creating socket...\n");
+    SOCKET socket_listen;
+    socket_listen = socket(bind_address->ai_family,
+            bind_address->ai_socktype, bind_address->ai_protocol);
+    if (!ISVALIDSOCKET(socket_listen)) {
+        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    printf("Binding socket to local address...\n");
+    if (bind(socket_listen,
+                bind_address->ai_addr, bind_address->ai_addrlen)) {
+        fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+    freeaddrinfo(bind_address);
+
+    printf("Listening...\n");
+    if (listen(socket_listen, 10) < 0) {
+        fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    fd_set master;
+    FD_ZERO(&master);
+    FD_SET(socket_listen, &master);
+    SOCKET max_socket = socket_listen;
+
+    printf("Waiting for connections...\n");
+
+    int n = 0, k = 100000;
+    const char *data = "201928492019284920192849";
+    const char *target = "7";
+    char rBuff[1024], wBuff[4096];
+    int state = 1;
+
+    while(state) {
+        fd_set reads;
+        reads = master;
+        if (select(max_socket+1, &reads, 0, 0, 0) < 0) {
+            fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
+            return 1;
+        }
+
+        SOCKET i;
+        for (i = 1; i <= max_socket; ++i) {
+            if (FD_ISSET(i, &reads)) {
+                if (i == socket_listen) {
+                    struct sockaddr_storage client_address;
+                    socklen_t client_len = sizeof(client_address);
+                    SOCKET socket_client = accept(socket_listen,
+                            (struct sockaddr*) &client_address,
+                            &client_len);
+                    if (!ISVALIDSOCKET(socket_client)) {
+                        fprintf(stderr, "accept() failed. (%d)\n",
+                                GETSOCKETERRNO());
+                        return 1;
+                    }
+
+                    FD_SET(socket_client, &master);
+                    if (socket_client > max_socket)
+                        max_socket = socket_client;
+                    
+                    printf("New connection from %d\n", socket_client);
+                    write(socket_client, data, strlen(data));
+                    write(socket_client, target, strlen(data));
+                    sleep(2);
+                    sprintf(wBuff, "%d", n);
+                    write(socket_client, wBuff, strlen(wBuff));
+                    n = n + 100000;
+                } else {
+                    int bytes_received = read(i, rBuff, 1024);
+                    if (bytes_received < 1) {
+                        sprintf(wBuff, "End connection from %d\n", i);
+                        FD_CLR(i, &master);
+                        CLOSESOCKET(i);
+                    } else {
+                        rBuff[bytes_received] = '\0';
+                    }
+
+                    printf("%s\n", rBuff);
+                    if (rBuff[0] == 'Y') {
+                        state = 0;
+                    } else {
+                        sprintf(wBuff, "%d", n);
+                        write(i, wBuff, strlen(wBuff));
+                        n = n + k;
+                    }                    
+                }
+            }
+        }
+    }
+
+    printf("Closing listening socket...\n");
+    CLOSESOCKET(socket_listen);
+
+    printf("Finished.\n");
+
+    return 0;
+}
