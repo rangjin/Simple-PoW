62e7b5bdd2c447a2fff682e1e80dac2a094b8d66
qualificationalitated <rlagusdn9548@gmail.com>
2023-06-16 17:26:35 +0900

    멀티플렉스 코드 주석 추가

--- multiplex_client.c
+++ multiplex_client.c
@@ -39,14 +39,17 @@
 #define SOCKET int
 #define GETSOCKETERRNO() (errno)
 
+// 입력 데이터와 목표값에 대한 Proof of Work 작업 수행
+// 작업 결과로 nonce 리턴
 unsigned long long proofOfWork(const char *data, int target, unsigned long long start) {
     unsigned long long nonce;
     char input[SHA256_DIGEST_LENGTH + sizeof(unsigned long long)];
     unsigned char hash[SHA256_DIGEST_LENGTH];
 
+    // 십만 범위의 반복을 통해 해시 계산
     for (nonce = start; nonce < start + 100000; nonce++) {
-        sprintf(input, "%s%llu", data, nonce);
-        SHA256(input, strlen(input), hash);
+        sprintf(input, "%s%llu", data, nonce); // input 문자열 생성
+        SHA256(input, strlen(input), hash); // 문자열 해시 계산
 
         int state = 1, k = target / 2;
         for (int i = 0; i < k; i++) {
@@ -58,7 +61,7 @@ unsigned long long proofOfWork(const char *data, int target, unsigned long long 
         if (target % 2 != 0 && hash[k] > 0x0F) {
             state = 0;
         }
-
+        // nonce 검증 성공
         if (state) {
             printf("%llu: ", nonce);
             for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
@@ -76,13 +79,16 @@ unsigned long long proofOfWork(const char *data, int target, unsigned long long 
 int main(int argc, char *argv[]) {
     char data[BUFSIZ], targetBuff[BUFSIZ];
     int dataLen, targetLen, target;
-
+    
+    // 프로그램 실행 인자 오류 처리
     if (argc < 3) {
         fprintf(stderr, "usage: tcp_client hostname port\n");
         return 1;
     }
 
     printf("Configuring remote address...\n");
+    // 원격지 주소 설정(서버)
+    // 연결할 서버의 IP 및 포트번호를 설정합니다.
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
     hints.ai_socktype = SOCK_STREAM;
@@ -92,6 +98,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // 원격 주소 출력
     printf("Remote address is: ");
     char address_buffer[100];
     char service_buffer[100];
@@ -101,26 +108,32 @@ int main(int argc, char *argv[]) {
             NI_NUMERICHOST);
     printf("%s %s\n", address_buffer, service_buffer);
 
+    // 소켓 생성
     printf("Creating socket...\n");
     SOCKET socket_peer;
     socket_peer = socket(peer_address->ai_family,
             peer_address->ai_socktype, peer_address->ai_protocol);
+    // 소켓 생성 실패시, 에러메세지 출력
     if (!ISVALIDSOCKET(socket_peer)) {
         fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
 
+    // 소켓을 원격 서버에 연결
     printf("Connecting...\n");
+    // 서버 연결 실패시, 에러메세지 출력
     if (connect(socket_peer,
                 peer_address->ai_addr, peer_address->ai_addrlen)) {
         fprintf(stderr, "connect() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
+    // 연결 성공시 peer_address 해제
     freeaddrinfo(peer_address);
 
+    // 서버로부터 데이터 수신(연결 가능 확인)
     dataLen = read(socket_peer, data, sizeof(data));
     data[dataLen] = '\0';
-
+    // 서버가 처리가능한 클라이언트 수를 넘어설경우, 에러메세지 출력
     if (strcmp(data, "Cannot connect to Server. It's fulled.\n") == 0) {
         printf("%s", data);
         CLOSESOCKET(socket_peer);
@@ -131,6 +144,7 @@ int main(int argc, char *argv[]) {
         printf("Connected.\n");
     }
 
+    // 서버로부터 목표값을 읽어와, atoi 함수를 이용해 정수로 변환 후 저장
     targetLen = read(socket_peer, targetBuff, sizeof(targetBuff));
     targetBuff[targetLen] = '\0';
     target = atoi(targetBuff);
@@ -139,17 +153,23 @@ int main(int argc, char *argv[]) {
 
     char rBuff[BUFSIZ], wBuff[BUFSIZ];
     unsigned long long ans, start;
-
+    
+    // 작업증명 작업 진행
     while(1) {
+        // 원격 서버로부터 데이터 획득
         int readLen = read(socket_peer, rBuff, sizeof(rBuff));
+        // 데이터 획득 실패시, 연결 종료
         if (readLen < 1) {
             printf("Connection closed by peer.\n");
             break;
         }
         rBuff[readLen] = '\0';
 
+        // 획득한 데이터를 부호없는 10진수로 변환해 저장.
         start = strtoull(rBuff, 0, 10);
         printf("%lld\n", start);
+
+        // 작업증명 수행
         ans = proofOfWork(data, target, start);
 
         if (ans == 0ULL) {
--- multiplex_server.c
+++ multiplex_server.c
@@ -41,11 +41,15 @@
 #define GETSOCKETERRNO() (errno)
 
 int main(int argc, char *argv[]) {
+    // 프로그램 실행 인자 확인
+    // 클라이언트 최대 갯수, data, 난이도를 인자로 입력
+    // 잘못된 인자 수 입력시 에러메세지 출력 후 종료
     if (argc < 4) {
         fprintf(stderr, "usage: client_num data target\n");
         return 1;
     }
 
+    // 로컬 주소 구성
     printf("Configuring local address...\n");
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
@@ -56,16 +60,20 @@ int main(int argc, char *argv[]) {
     struct addrinfo *bind_address;
     getaddrinfo(0, "8080", &hints, &bind_address);
 
+    // 소켓 생성
     printf("Creating socket...\n");
     SOCKET socket_listen;
     socket_listen = socket(bind_address->ai_family,
             bind_address->ai_socktype, bind_address->ai_protocol);
+    // 소켓이 생성되지 않았거나, 유효하지 않다면 에러메세지 출력
     if (!ISVALIDSOCKET(socket_listen)) {
         fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
 
+    // 소켓 바인딩
     printf("Binding socket to local address...\n");
+    // 소켓 바인딩이 실패할경우, 에러메세지 출력
     if (bind(socket_listen,
                 bind_address->ai_addr, bind_address->ai_addrlen)) {
         fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
@@ -73,19 +81,23 @@ int main(int argc, char *argv[]) {
     }
     freeaddrinfo(bind_address);
 
+    // 클라이언트 연결 대기, 최대 10개의 클라이언트를 받도록 설정
+    // 연결 대기 실패시, 에러메세지 출력 후 프로그램 종료
     printf("Listening...\n");
     if (listen(socket_listen, 10) < 0) {
         fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
 
+    // select 연산을 위해, fd_set 구조체 선언 후 초기화
     fd_set master;
     FD_ZERO(&master);
     FD_SET(socket_listen, &master);
     SOCKET max_socket = socket_listen;
 
     printf("Waiting for connections...\n");
-
+    
+    // 작업증명 초기화
     unsigned long long n, k = 100000L;
     int cnt = 0, max = atoi(argv[1]);
     const char *data = argv[2];
@@ -97,27 +109,34 @@ int main(int argc, char *argv[]) {
     while(state) {
         fd_set reads;
         reads = master;
+        // select 함수로 클라이언트 연결 요청 확인
+        // select 동작시 에러가 발생할경우, 에러메세지 출력
         if (select(max_socket+1, &reads, 0, 0, 0) < 0) {
             fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
             return 1;
         }
 
+        // 선형적으로 탐색하며 클라이언트 소켓에 대한 이벤트 확인
         SOCKET i;
         for (i = 1; i <= max_socket; ++i) {
             if (FD_ISSET(i, &reads)) {
+                // 새로운 소켓에 대한 연결인경우, 클라이언트 연결 수락
                 if (i == socket_listen) {
                     struct sockaddr_storage client_address;
                     socklen_t client_len = sizeof(client_address);
+                    // accept 함수로 클라이언트 연결 수락
                     SOCKET socket_client = accept(socket_listen,
                             (struct sockaddr*) &client_address,
                             &client_len);
+                    // socket_client가 유효하지 않은경우, 에러메세지를 출력하고 프로그램 종료
                     if (!ISVALIDSOCKET(socket_client)) {
                         fprintf(stderr, "accept() failed. (%d)\n",
                                 GETSOCKETERRNO());
                         return 1;
                     }
-
+                    // 연결 가능한 클라이언트 수가 남아있는 경우
                     if (cnt < max) {
+                        // socket_client 소켓을 master에 추가 후, 최대 소켓 번호 업데이트
                         FD_SET(socket_client, &master);
                         if (socket_client > max_socket)
                             max_socket = socket_client;
@@ -127,13 +146,16 @@ int main(int argc, char *argv[]) {
                         write(socket_client, data, strlen(data));
                         sleep(1);
                         write(socket_client, target, strlen(data));
-                    } else {
+                    } 
+                    // 연결 가능한 클라이언트 수가 모자란경우
+                    else {
                         sprintf(wBuff, "Cannot connect to Server. It's fulled.\n");
                         write(socket_client, wBuff, strlen(wBuff));
                         CLOSESOCKET(socket_client);
                         continue;
                     }
 
+                    // 작업증명이 처음 시작되는 경우, 시작 시간 저장
                     if (n == 0 && cnt == max) {
                         printf("start!!!\n");
                         sleep(1);
@@ -146,8 +168,12 @@ int main(int argc, char *argv[]) {
                             }
                         }
                     }
-                } else {
+                } 
+                // 클라이언트 소켓에 대한 요청 처리
+                else {
                     int bytes_received = read(i, rBuff, 1024);
+                    // 클라이언트에서 전송한 데이터 확인
+                    // 클라이언트로의 입력이 진행되지 않는경우, 연결 종료
                     if (bytes_received < 1) {
                         sprintf(wBuff, "End connection from %d\n", i);
                         FD_CLR(i, &master);
@@ -156,6 +182,7 @@ int main(int argc, char *argv[]) {
                         rBuff[bytes_received] = '\0';
                     }
 
+                    // Y를 수신한경우, 작업증명 종료임을 판단하고, end 시간 저장
                     printf("%s\n", rBuff);
                     if (rBuff[0] == 'Y') {
                         state = 0;
@@ -165,7 +192,9 @@ int main(int argc, char *argv[]) {
                                 CLOSESOCKET(j);
                             }
                         }
-                    } else {
+                    } 
+                    // 그렇지 않은 경우, 다음번 탐색을 시작할 수 증가.
+                    else {
                         sprintf(wBuff, "%llu", n);
                         write(i, wBuff, strlen(wBuff));
                         n = n + k;
@@ -174,7 +203,8 @@ int main(int argc, char *argv[]) {
             }
         }
     }
-
+    
+    // 실행시간 출력 후 소켓 닫기
     printf("time spent: %lf\n", (double) end - (double) begin);
     printf("Closing listening socket...\n");
     CLOSESOCKET(socket_listen);
